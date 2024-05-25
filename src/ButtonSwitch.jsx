29a5e83a90619ffa1c421382986b291a329f4794
Guillermo <guillermo.gil.cor@gmail.com>
2024-05-25 10:33:18 +0100

    first commit

---
+++ src/ButtonSwitch.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ButtonSwitch = ({ luz="red", setLuz }) => {
+    const alternate = () => {
+      if (luz === "red") {
+        setLuz("orange");
+      } else if (luz === "orange") {
+         setLuz("green");
+      } else {
+        setLuz("red");
+      }
+    };
+
+  return (
+    <div className="grid">
+      <button onClick={alternate} className="justify-self-center mt-5 bg-pink-900 p-2 rounded-lg text-gray-50">
+        Alternar
+      </button>
+    </div>
+  );
+};
+
+export default ButtonSwitch;
---
+++ src/Semaforo.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import ButtonSwitch from "./ButtonSwitch";
+
+const Semaforo = () => {
+  const [luz, setLuz] = useState("red");
+
+  const changeLuz = (event) => {
+    const color = event.target.id;
+    if (color === "orange") {
+      setLuz("orange");
+    } else if (color === "green") {
+      setLuz("green");
+    } else {
+      setLuz("red");
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("parpadeo");
+
+      if (luz === "orange") {
+        setLuz("off");
+      } else if (luz === "off") {
+        setLuz("orange");
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [luz]);
+
+  return (
+    <>
+      <div className="grid justify-center">
+        <div className="bg-black h-20 w-5 justify-self-center"></div>
+        <ul className="bg-black p-4 grid gap-4 mx-10 h-64 w-32 justify-center rounded-xl">
+          <li
+            id="red"
+            className={`rounded-full size-16 ${
+              luz === "red" ? "bg-red-600" : "bg-red-950"
+            }`}
+            onClick={changeLuz}
+          ></li>
+          <li
+            id="orange"
+            className={`rounded-full size-16 ${
+              luz === "orange" ? "bg-orange-600" : "bg-orange-950"
+            }`}
+            onClick={changeLuz}
+          ></li>
+          <li
+            id="green"
+            className={`rounded-full size-16 ${
+              luz === "green" ? "bg-green-600" : "bg-green-950"
+            }`}
+            onClick={changeLuz}
+          ></li>
+        </ul>
+      </div>
+      <ButtonSwitch luz={luz} setLuz={setLuz} />
+    </>
+  );
+};
+
+export default Semaforo;
